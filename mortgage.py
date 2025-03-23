214c5f3fc7a274fdebae2d5eb4bbbbedc0f3187f
Jermit <jermitgunning@gmail.com>
2025-03-23 14:44:09 -0400

    Update mortgage.py

--- mortgage.py
+++ mortgage.py
@@ -24,7 +24,7 @@ import math
 st.title("Loan Repayments Calculator")
 st.sidebar.header("Designed by Jermit Gunning")
 st.subheader("Contact me here")
-st.info("I'm a passionate analyst dedicated to turning data into information and bridging the gap between technical and business teams for solutions that matter. I enjoy cross-functional teamwork to solve tough business problems and improve processes. Learning and self-improvement are cornerstones of both my work and personal life. ")
+st.sidebar.info("I'm a passionate analyst dedicated to turning data into information and bridging the gap between technical and business teams for solutions that matter. I enjoy cross-functional teamwork to solve tough business problems and improve processes. Learning and self-improvement are cornerstones of both my work and personal life. ")
 
 
 datar = [1,2,3,4]
